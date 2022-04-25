cfe6285019ffb4c38dcb74fe56c48ec952242839
Juani50 <juanibiondi50@gmail.com>
2022-04-25 16:02:13 -0300

    primer homehomrk

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,11 +3,28 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  var suma = 0;
+  for (var i = 0; i < num.length; i++){
+    suma += +num[i] * 2 ** (num.length -1 -i);
+   }
+   return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var bin = [];
+  for (var i = num; i > 0;){
+    bin.unshift(i % 2)
+    i = Math.floor(i / 2);
+  }
+  return bin.join ("");
 }
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,
